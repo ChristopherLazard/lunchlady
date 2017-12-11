e91888f6e9b7792c033d45d05f653a535d31b678
Christopher Lazard <christopher.l.lazard@gmail.com>
2017-12-11 10:18:32 -0700

    lunchlady

---
+++ lunchlady.rb
@@ -0,0 +1,14 @@
+diner
+
+
+def menu 
+  puts "What will your main dish be today?"
+  puts "1) dishes"
+  puts "2) dishes"
+  puts "3) dishes"
+  puts "4) dishes"
+  user_input = gets.to_i
+
+  end
+
+diner
